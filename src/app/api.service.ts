c8e1645eae8339a4cde9f11f5c0559cc7b2e5370
Frank van Wijk <fvanwijk@xebia.com>
2016-11-28 23:13:37 +0100

    Fetch user data and show in component

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ApiService {
   host = 'http://localhost:1337';
 
-  constructor() {
+  constructor(private http: Http) {}
+
+  getUser(wid): Observable<any> {
+    return this.http.get(`/api/wids/${wid}`)
+      .map(res => res.json());
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiService } from "./api.service";
 
 @Component({
@@ -6,10 +6,22 @@ import { ApiService } from "./api.service";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  host;
+export class AppComponent implements OnInit {
+  host = '';
+  user = undefined;
 
-  constructor(apiService: ApiService) {
+  constructor(private apiService: ApiService) {
     this.host = apiService.host;
+  }
+
+  getUserData() {
+    this.apiService.getUser('y1mGy1QTqtD')
+      .subscribe(user => {
+        this.user = user;
+      });
+  }
+
+  ngOnInit() {
+    this.getUserData();
   }
 }
