71d7201f3ff03e6da7bfaf1d1fb19fb9f7466a92
Frank van Wijk <fvanwijk@xebia.com>
2016-11-30 22:10:03 +0100

    Add two routes and a router outlet (home and planets)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
+import { ApiService } from './api.service';
 import { AppComponent } from './app.component';
-import {ApiService} from "./api.service";
+import { HomeComponent } from './home/home.component';
+import { PlanetListComponent } from './planets/planet-list/planet-list.component';
+import { PlanetService } from './planets/planet.service';
+
+const appRoutes: Routes = [
+  { path: 'planets', component: PlanetListComponent },
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PlanetListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
