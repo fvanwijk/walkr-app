b6416cc9f336e8a0fa9fee4d9a2e3bed21d57272
Frank van Wijk <fvanwijk@xebia.com>
2016-12-01 00:46:40 +0100

    Basic way to show results when API is called

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  apiUrl: String = 'http://localhost:4200/api/';
+  placeholder: String = 'planets/24';
+  query: String;
+  result: String;
 
-  constructor() { }
+  constructor(private http: Http) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSubmit() {
+    this.http.get(this.apiUrl + (this.query || this.placeholder))
+      .map(res => res.json())
+      .subscribe(results => {
+        this.result = JSON.stringify(results, undefined, 2);
+      });
   }
 
 }
