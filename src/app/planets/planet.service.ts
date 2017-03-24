ae6ea0cc4de5f19a17905c8e3fb78135f95f787c
Frank van Wijk <fvanwijk@xebia.com>
2017-03-24 22:59:15 +0100

    Fetch planet details and merge them into the original observable

--- src/app/planets/planet.service.ts
+++ src/app/planets/planet.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable } from "rxjs";
+import { Observable } from 'rxjs';
 import { Http } from '@angular/http';
+import { Planet } from './planet';
 
 @Injectable()
 export class PlanetService {
@@ -8,7 +9,17 @@ export class PlanetService {
   constructor(private http: Http) { }
 
   getPlanets(): Observable<any> {
-    return this.http.get('/api/planets')
-      .map(res => res.json());
+    return this.http.get('/api/planets').map(res => res.json())
+      .mergeMap((data: { results: Array<Planet> }) => {
+        return Observable.from(data.results)
+          //.filter((_, i) => i < 3) // Do not fetch too much for debugging
+          .mergeScan((acc, litePlanet) => {
+            return this.http.get(litePlanet.url.replace('1337', '4200')).map(res => res.json())
+              .concatMap(planet => {
+                acc.push(planet);
+                return Observable.of(acc);
+              });
+          }, []);
+      });
   }
 }
