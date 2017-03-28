d86bfb1ae60f5d1d72f483c499bf88142ff98149
Frank van Wijk <fvanwijk@xebia.com>
2017-03-28 19:05:21 +0200

    Fetch details of discoveries

--- src/app/planets/my-planets/my-planets-resolve.service.ts
+++ src/app/planets/my-planets/my-planets-resolve.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
-import { Discovery } from '../planet';
+import { Discovery, Planet } from '../planet';
 import { PlanetService } from '../planet.service';
 
 @Injectable()
 export class MyPlanetsResolve implements Resolve<[Discovery]|boolean> {
-  constructor(private ps: PlanetService) {}
+  constructor(private http: Http, private ps: PlanetService) {}
   resolve(route: ActivatedRouteSnapshot): Observable<[Discovery]>|boolean {
-    return this.ps.getDiscoveredPlanets('y1mGy1QTqtD');
+    return this.ps.getDiscoveredPlanets('y1mGy1QTqtD')
+      .mergeMap((discoveries: [Discovery]) => {
+        return Observable.from(discoveries)
+          .mergeScan((acc, liteDiscovery) => {
+            return this.http.get(liteDiscovery.url.replace('1337', '4200')).map(res => res.json())
+              .mergeMap(discovery => {
+                acc.push(discovery);
+                return Observable.of(acc);
+              })
+          }, []);
+      });
   }
 }
