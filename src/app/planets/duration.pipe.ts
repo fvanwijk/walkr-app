7ed032c58683c6b7550f7a4343077cee50541071
Frank van Wijk <fvanwijk@xebia.com>
2017-08-03 08:24:56 +0200

    Add discovery durationn

--- src/app/planets/duration.pipe.ts
+++ src/app/planets/duration.pipe.ts
@@ -6,6 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DurationPipe implements PipeTransform {
 
   transform(value: any, args?: any): String {
+    if (!value) {
+      return '-';
+    }
     const hours: number = Math.floor(value / 3600);
     let minutes: number = Math.floor((value - (hours * 3600)) / 60);
     let seconds: number = value - (hours * 3600) - (minutes * 60);
